f6a9870a9d174571cc66c04b8a4dd9a89896e6e4
Timotej Sofijanović <76438562+stimotej@users.noreply.github.com>
2025-10-03 19:12:02 +0200

    Added base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/teatar-td",
   images: {
     remotePatterns: [
       new URL("https://www.sczg.unizg.hr/wp-content/uploads/**"),
